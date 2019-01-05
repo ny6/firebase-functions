dad834ca5978831208cdeaa0027bbceaf2b06ed5
Yeshu Kochher <yeshu@outlook.in>
2019-01-05 07:35:01 +0530

    one where more improvements in error handling

--- functions/create_user.js
+++ functions/create_user.js
@@ -10,9 +10,22 @@ module.exports = async (req, res) => {
     if (!uid || uid.length !== 10) throw ykError('Invalid phone number!', 422);
 
     const user = await auth().createUser({ uid });
+    if (!user) throw ykError('Unable to create user!', 500);
 
-    return res.send({ status: true, data: { user } });
-  } catch ({ message, code = 400 }) {
-    return res.status(code).send({ status: false, message });
+    return res.send({
+      status: true,
+      message: 'User registered successfully!',
+      data: { phone: user.uid },
+    });
+  } catch (err) {
+    const { code } = err;
+    let { message } = err;
+    let statusCode = typeof code === 'number' ? code : 500;
+
+    if (code === 'auth/uid-already-exists') {
+      message = 'Phone number already exists!';
+      statusCode = 400;
+    }
+    return res.status(statusCode).send({ status: false, message });
   }
 };
